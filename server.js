e961914242f03d57561eff812a465fea1408a46f
ScarFace <aryansingh272001@gmail.com>
2026-05-12 19:07:28 +0530

    added root route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req,res)=>{
+   res.send("Backend API Running Successfully 🚀")
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
